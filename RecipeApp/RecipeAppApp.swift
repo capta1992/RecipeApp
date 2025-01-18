9a33be16a11167966c63fdd662e7e7ccc54a9db6
capta1992 <47236560+capta1992@users.noreply.github.com>
2025-01-17 22:44:16 -0600

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Lawson Falomo on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
